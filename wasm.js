3c18e2b238c3bd1ff65b77c14e5c66f364d7334c
Jiayi Zhao <jeff.no.zhao@gmail.com>
2020-12-31 21:13:58 -0500

    update website

--- wasm.js
+++ wasm.js
@@ -563,16 +563,16 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper479 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 327, __wbg_adapter_22);
+    imports.wbg.__wbindgen_closure_wrapper472 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 321, __wbg_adapter_22);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper880 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 430, __wbg_adapter_25);
+    imports.wbg.__wbindgen_closure_wrapper879 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 429, __wbg_adapter_25);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper927 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 446, __wbg_adapter_28);
+    imports.wbg.__wbindgen_closure_wrapper926 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 445, __wbg_adapter_28);
         return addHeapObject(ret);
     };
 
